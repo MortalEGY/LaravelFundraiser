3563700b2a34f3be7aa9947d298342bae81b84c8
Omar <omarbomb11@gmail.com>
2022-09-14 17:17:29 +0200

    First Commit

---
+++ app/Mail/MailVerification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MailVerification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $token;
+
+    public $subject = 'Welcome to Fundraiser, please verify your account';
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $link = route('auth.verification', ['token' => $this->token]);
+        return $this ->subject('Mail from Laravel Fundraisers')
+                     ->view('mail.account_verification', compact('link'));
+    }
+}
---
+++ resources/views/mail/account_verification.blade.php
@@ -0,0 +1,6 @@
+<p>
+    Please click here to activate your account:
+    <br>
+    <a href="{{ $link }}">{{ $link }}</a>
+    Thank you for joining !!
+</p>
